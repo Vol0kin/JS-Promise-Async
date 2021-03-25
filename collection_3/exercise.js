4b764a14b8e81ea5c68ddf771af080072c4b1003
Vladislav Nikolov <vladis890@gmail.com>
2021-03-25 13:43:14 +0100

    Finish exercise from collection 3

---
+++ collection_3/exercise.js
@@ -0,0 +1,24 @@
+/*
+  Get the same behavior in next code, removing 'then' expressions.
+
+*/
+function getCountry(){
+    return fetch('https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/471a2e653140ecdd7243cdcacfd66608/country-codes_json.json')
+        
+}
+
+/*getCountry().then(response => response.json())
+    .then(response => response.map(country => country['CLDR display name']))
+    .then(response => console.log(response));*/
+
+(async () => {
+  try {
+    const response = await getCountry();
+    const jsonResponse = await response.json();
+    const countryNames = await jsonResponse.map(country => country['CLDR display name']);
+    
+    console.log(countryNames);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
